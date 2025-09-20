1e4f7630b416da97d3d6c4016bd869a9b99ff74e
atularorajaipur-coder <atularora.jaipur@gmail.com>
2025-09-20 15:21:59 +0530

    Add files via upload

---
+++ src/test/java/com/soron/bot/whatsappbot/WhatsappbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soron.bot.whatsappbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
